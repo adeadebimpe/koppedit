8dc33a78648967931b2be90dd2373bce98eeeadd
Adebimpe Adebowale <adebimpe@Adebimpes-MacBook-Pro-2.local>
2020-09-02 15:52:48 +0100

    pushed faq

--- src/components/faq/faqitem.js
+++ src/components/faq/faqitem.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown';
 
 const FaqItem = ({ faq, toggleFaq }) => {
   return (
     <div className={"faq__item " + (faq.open ? "open" : "")} onClick={toggleFaq} >
     
     <h6 className="faq__question" >{faq.node.question}</h6>
-    <p className="faq__answer">{faq.node.answer.answer}</p>
+    <div className="faq__answer">
+    <ReactMarkdown source={faq.node.answer.answer} escapeHtml={false} />
+    </div>
 
     </div>
   )
