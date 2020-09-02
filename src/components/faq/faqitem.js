35a564b0c5bf161d47ffe5d21c6855bc5834d571
Adebimpe Adebowale <adebimpe@Adebimpes-MacBook-Pro-2.local>
2020-09-02 20:57:20 +0100

    made changes

--- src/components/faq/faqitem.js
+++ src/components/faq/faqitem.js
@@ -3,11 +3,11 @@ import ReactMarkdown from 'react-markdown';
 
 const FaqItem = ({ faq, toggleFaq }) => {
   return (
-    <div className={"faq__item " + (faq.open ? "open" : "")} onClick={toggleFaq} >
+    <div className={"faq__item " + (faq.open ? "open" : "")} onClick={toggleFaq} onBlur={toggleFaq} >
     
-    <h6 className="faq__question" >{faq.node.question}</h6>
+    <button className="faq__question" >{faq.node.question}</button>
     <div className="faq__answer">
-    <ReactMarkdown source={faq.node.answer.answer} escapeHtml={false} />
+     <ReactMarkdown source={faq.node.answer.answer} escapeHtml={false} />
     </div>
 
     </div>
